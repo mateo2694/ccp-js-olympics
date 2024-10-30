d660222111564b1e792c34f23a73b772d05e6f8a
Mateo Sánchez <mateosm@outlook.com>
2024-10-30 08:24:13 -0500

    Add http requests

--- src/main.js
+++ src/main.js
@@ -1,2 +1,67 @@
-console.log("Hello World!");
-console.log(document.body);
+// Encontrar botones
+const getButton = document.getElementById("getButton");
+const postButton = document.getElementById("postButton");
+
+// Encontrar contenedor para la imagen
+const imageContainer = document.getElementById("imageContainer");
+
+// Encontrar la lista de resultados
+const list = document.getElementById("list");
+
+// Variable para almacenar la imagen
+let image;
+
+// Petición para obtener una imagen aleatoria
+getButton.addEventListener("click", async function () {
+  const response = await fetch(
+    "https://api.api-ninjas.com/v1/randomimage?category=technology",
+    {
+      method: "GET",
+      headers: {
+        "X-Api-Key": "GfSyTfGfEt05R2sUss4wmg==yLMchIkKLkuL6cSm",
+        Accept: "image/jpg",
+      },
+    }
+  );
+
+  console.log("GET", response);
+
+  if (response.ok === true) {
+    image = await response.blob();
+    const imageUrl = URL.createObjectURL(image);
+    imageContainer.src = imageUrl;
+  }
+});
+
+// Petición para detectar objetos en la imagen
+postButton.addEventListener("click", async function () {
+  const formData = new FormData();
+  formData.append("image", image);
+
+  const response = await fetch(
+    "https://api.api-ninjas.com/v1/objectdetection",
+    {
+      method: "POST",
+      headers: {
+        "X-Api-Key": "GfSyTfGfEt05R2sUss4wmg==yLMchIkKLkuL6cSm",
+      },
+      body: formData,
+    }
+  );
+
+  console.log("POST", response);
+  const results = await response.json();
+  console.log("results", results);
+
+  if (response.ok === true) {
+    const labels = results.map((result) => result.label);
+    const labelSet = new Set(labels);
+    let listItems = "";
+
+    labelSet.forEach(function (label) {
+      listItems = listItems + `<li>${label}</li>`;
+    });
+
+    list.innerHTML = listItems;
+  }
+});
