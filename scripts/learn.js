704193f1c960dd44af9d2fbdc4cc96a3f4841ae6
Mateo Sánchez <mateosm@outlook.com>
2024-10-30 12:40:15 -0500

    Write down some exercises

--- scripts/learn.js
+++ scripts/learn.js
@@ -1,1 +1,23 @@
-console.log("Hello World!!");
+console.log("Hello GIRL POWER!");
+
+// Console log
+// * ¡Muestra tu propio mensaje en la consola!
+
+// Variables y tipos de datos
+// * ¡EXPLOREMOS!
+
+// Funciones
+// * Escribe una función que reciba dos números y devuelva su suma
+// * Escribe una función que reciba dos valores y devuelva true si ambos son iguales y del mismo tipo
+
+// Arreglos
+// * Escribe una función que reciba un arreglo (a) y un número (n), y devuelva el elemento del arreglo que se encuentra en la posición n
+
+// Objetos
+// * Escribe una función que reciba un objeto y un string, y devuelva true si existe una propiedad cuyo nombre y valor sean iguales al string
+// * Escribe una función que reciba un objeto y un string, y devuelva true si existe una propiedad con ese nombre (no importa su valor)
+
+// Loops
+// * Escribe una función que reciba un arreglo de números y devuelva la suma de todos los números
+// * Escribe una función que reciba un arreglo de números (a) y un númnero (n), y devuelva la suma de todos los números mayores que n
+// * Escribe una función que reciba un arreglo de números y devuelva el promedio de todos los números
